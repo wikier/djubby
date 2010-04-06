d838956f204263b9ed973922f49dc8d9e687d02e
Sergio Fernandez <sergio@wikier.org>
2010-04-06 12:00:04 +0200

    license

--- lib/setup.py
+++ lib/setup.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# This file is part of Djubby.
+#
+# Djubby is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Djubby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Djubby. If not, see <http://www.gnu.org/licenses/>.
+
+
 from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
